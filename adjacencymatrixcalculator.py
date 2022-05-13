f52f2abcfcc0f4c5b4ea7e5847865da657c93418
Zachary Ringwood <zringwood@gmail.com>
2022-05-13 13:48:38 -0400

    Add files via upload
    Generates the adjacency matrix of a graph such that each vertex represents a letter and position in a given wordle guess. a in position 1 = 0, a in position 2 = 26, and so on. Two vertices share an edge weighted according to the number of times they appear in the same guess.

---
+++ adjacencymatrixcalculator.py
@@ -0,0 +1,26 @@
+#Writes an adjacency matrix for the following graph:
+#Each Vertex represents a letter of the alphabet in a particular place in a wordle guess
+#Verticies share an edge IFF the letters they represent are in the same word.
+legalguesses = open("wordlelegalguesses.txt", "r")
+
+graph = [[0] * 26 for i in range(26*5)]
+
+#For every legal guess in the file, we want to add one edge for letter in the word
+nextword = legalguesses.readline()
+while nextword != "" :
+    for i in range(len(nextword)-1):
+        for j in range(len(nextword)-1):
+            if i != j : 
+                graph[ord(nextword[i]) - ord('a') + i*26][ord(nextword[j]) - ord('a')] += 1
+    nextword = legalguesses.readline()
+#Writes the graph to a string
+graphstring = "["
+for vertex in graph :
+    graphstring += "{},\n".format(vertex)
+#There's an extra comma on the end of the string, this cuts it off
+graphstring = graphstring[:len(graphstring)-2]
+graphstring += "]"
+guessesgraph = open("guessesgraph.txt","w")
+guessesgraph.write(graphstring)
+guessesgraph.close()
+
